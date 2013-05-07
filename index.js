2eabeff16f4dd66836bbc8288de8b15d40b257ee
Prashant Singh <prashant@codeforamerica.org>
2013-05-07 14:44:28 -0700

    Refactor a bit
    Use Archiver, the apparent successor to zip-stream.
    Use node 0.10, so we can more safely mix streams and promises.
    Add skeleton of inversion-of-control interface.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var childProcess = require('child_process');
 
 var express = require('express');
-var zipstream = require('zipstream');
+var archiver = require('archiver');
 var uuid = require('node-uuid');
 var Q = require('q');
 var qfs = require('q-io/fs');
@@ -57,6 +57,64 @@ app.get('/ogr2ogr', function (req, res) {
   });
 });
 
+/**
+ * @return {Promise} A promise for a Readable Stream for the zipfile.
+ */
+function convert(geoJSONFile) {
+  var id = uuid.v1();
+  var outdir = TMPDIR + '/' + id;
+  var outname = 'output';
+  var zip;
+
+  // Make the temporary directory
+  return qfs.makeDirectory(outdir)
+  .then(function () {
+    // Run the ogr2ogr command
+    return exec('LD_LIBRARY_PATH=/app/vendor/gdal/lib GDAL_DATA=/app/vendor/gdal/share/gdal /app/vendor/gdal/bin/ogr2ogr -f "ESRI Shapefile" ' + outdir + '/' + outname + '.shp ' + geoJSONFile);
+  })
+  .then(function (outputs) {
+    // Gather the names of the files created by ogr2ogr.
+
+    console.log(outputs.stdout);
+    console.log(outputs.stderr);
+
+    // Return a promise for the file names
+    return qfs.list(outdir);
+  })
+  .then(function (files) {
+    zip = archiver('zip');
+
+    zip.on('error', function (error) {
+      console.log(error);
+      throw error;
+    });
+
+    // Add files to the zip stream.
+    files.forEach(function (name) {
+      zip.append(fs.createReadStream(outdir + '/' + name), { name : name });
+    });
+
+    // Finalize the zip stream.
+    Q.ninvoke(zip, 'finalize')
+    .then(function (count) {
+      console.log('Wrote ' + count + ' to the ZIP archive.');
+      // Remove temporary directory
+      return qfs.removeTree(outdir);
+    })
+    .then(function () {
+      // Remove the temporary GeoJSON file
+      return qfs.remove(geoJSONFile);
+    })
+    .fail(function (error) {
+      console.log(error);
+      throw error;
+    });
+
+    // Return a promise for the ZIP stream.
+    return zip;
+  });
+}
+
 function postGeoJSON2Shapefile(req, res) {
   var id = uuid.v1();
   var geoJSONFile = TMPDIR + '/' + id + '.json';
@@ -71,73 +129,38 @@ function postGeoJSON2Shapefile(req, res) {
   // Save the GeoJSON input to a temporary file.
   qfs.write(geoJSONFile, req.body)
   .then(function () {
-    // Make the temporary directory
-    return qfs.makeDirectory(outdir);
+    return convert(geoJSONFile);
   })
-  .then(function () {
-    // Run the ogr2ogr command
-    return exec('LD_LIBRARY_PATH=/app/vendor/gdal/lib GDAL_DATA=/app/vendor/gdal/share/gdal /app/vendor/gdal/bin/ogr2ogr -f "ESRI Shapefile" ' + outdir + '/' + outname + '.shp ' + geoJSONFile);
-  })
-  .then(function (outputs) {
-    console.log(outputs.stdout);
-    console.log(outputs.stderr);
-
+  .then(function (zip) {
     res.set('Content-Type', 'application/zip');
-
-    // Pipe the zip file to the response.
-    zip = zipstream.createZip();
     zip.pipe(res);
-
-    // Return a promise for the file names
-    return qfs.list(outdir);
-  })
-  .then(function (files) {
-    // Add files to the zip stream.
-
-    // Make functions that each add a file to the zip stream and return a promise.
-    var adders = files.map(function (name) {
-      return function add() {
-        var deferred = Q.defer();
-        zip.addFile(fs.createReadStream(outdir + '/' + name), { name : name }, function () {
-          deferred.resolve();
-        });
-        return deferred.promise;
-      }
-    });
-
-    // Sequence the additions of files to the zip stream.
-    return adders.reduce(function (promise, f) {
-      return promise.then(f);
-    }, Q.resolve());
-  })
-  .then(function () {
-    var deferred = Q.defer();
-    // Finalize the zip stream
-    zip.finalize(function (count) {
-      deferred.resolve(count);
-    });
-    return deferred.promise;
-  })
-  .then(function (count) {
-    console.log(count + ' total bytes written');
-    // End the response
-    res.end();
-
-    // Remove temporary directory
-    return qfs.removeTree(outdir);
-  })
-  .then(function () {
-    // Remove the temporary GeoJSON file
-    return qfs.remove(geoJSONFile);
   })
   .fail(function (error) {
     console.log(error);
     res.send(500);
+    throw error;
   });
+}
+
+function handleControlInversion(req, res) {
+  var url = req.body.url;
+
+  // Determine S3 path
+  // XXX
+  // Send S3 path to client
+  // XXX
+  // Fetch GeoJSON data
+  // XXX
+  // Convert to shapefile
+  // XXX
+  // Store on S3
+  // XXX
+ res.send(501);
 }
 
 app.post('/geojson2shp', bufferPostData, postGeoJSON2Shapefile);
 app.post('/geojson2shp/:basename', bufferPostData, postGeoJSON2Shapefile);
+app.post('/inversion/geojson2shp/:basename', express.json(), handleControlInversion);
 
 server.listen(process.env.PORT, function (error) {
   if (error) {
